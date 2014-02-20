9648bb6742a52901d84e59110279179b63ea3d74
Francois Greze <francois.greze@devmeup.net>
2014-02-20 17:02:33 +0100

    added revolution-slider

--- js/main.js
+++ js/main.js
@@ -14,12 +14,34 @@ $(window).load(function() {
     //scrollIt
     $(this).scrollTop(0);
     $.scrollIt();
+
+
+
 });
- $(window).bind('scroll', function() {
-         if ($(window).scrollTop() > 1200) {
-             $('.navbar').css('position', 'fixed');
-         }
-         else {
-             $('.navbar').css('position', 'relative');
-         }
-    });
+$(window).bind('scroll', function() {
+    if ($(window).scrollTop() > 940) {
+        $('.navbar').css('position', 'fixed');
+    }
+    else {
+        $('.navbar').css('position', 'relative');
+    }
+});
+
+var revapi;
+
+jQuery(document).ready(function() {
+
+    revapi = jQuery('.tp-banner').revolution(
+            {
+                delay: 15000,
+                startwidth: 940,
+                startheight: 1200,
+                hideThumbs: 0,
+                autoHeight:"on",
+                onHoverStop:"off",
+                fullWidth: "on",
+                fullScreen: "on",
+                navigationStyle: "none",
+            });
+
+});	//ready
